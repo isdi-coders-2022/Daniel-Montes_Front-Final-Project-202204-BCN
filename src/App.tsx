9ceda8b5688c22865118dc00e6a4659c78ebf15a
Daniel Montes <daniel.montes@gmail.com>
2022-06-01 01:51:30 +0200

    Add login skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App" />;
-}
+const App: React.FC = () => {
+  return <h1>AdoptaUnPinguino.com</h1>;
+};
 
 export default App;
---
+++ src/app/redux/features/userSlice/userSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../store/store";
+import { LoginResponse } from "../../types/userInterfaces";
+
+const initialState: LoginResponse = {
+  name: "",
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    login: (
+      currentState: LoginResponse,
+      action: PayloadAction<LoginResponse>
+    ) => {
+      return { ...action.payload };
+    },
+    logout: () => initialState,
+  },
+});
+
+export const { login: loginActionCreator, logout: logoutActionCreator } =
+  userSlice.actions;
+export const selectCount = (state: RootState) => state.users;
+
+export default userSlice.reducer;
