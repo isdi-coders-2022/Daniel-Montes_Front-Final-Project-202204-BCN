de74ec4edf0761ff50cee461bc29ae03739a7a6b
Daniel Montes <daniel.montes@gmail.com>
2022-06-01 13:49:23 +0200

    Add test for LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import LoginForm from "./components/LoginForm/LoginForm";
+import LoginPage from "./pages/loginPage";
 
 const App: React.FC = () => {
   return (
     <>
       <h1>AdoptaUnPinguino.com</h1>
-      <LoginForm />
+      <LoginPage />
     </>
   );
 };
