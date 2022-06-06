2a883a208a327c932587c7fc0e5bef8c265dcb93
Daniel Montes <daniel.montes@gmail.com>
2022-06-06 12:16:46 +0200

    Add penguins page styles

--- src/components/Penguin/Penguin.tsx
+++ src/components/Penguin/Penguin.tsx
@@ -1,4 +1,6 @@
 import { IPenguin } from "../../app/redux/types/penguin/penguinInterfaces";
+import starImage from "../../images/star-empty.png";
+import likesImage from "../../images/heart-empty.png";
 
 const Penguin = ({
   penguin: { id, name, category, image, likes, description },
@@ -7,13 +9,19 @@ const Penguin = ({
 }) => {
   return (
     <div className="penguin-container">
+      <div className="penguin-title">
+        <h2 className="penguin-name">{name}</h2>
+        <img src={starImage} className="star-image" alt={name} />
+      </div>
       <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <ul className="penguin-datalist">
-        <li>{category}</li>
-        <li>{likes}</li>
-        <li>{description}</li>
-      </ul>
+      <div className="penguin-datalist">
+        <span className="category">{category}</span>
+        <span className="likes">{likes}</span>
+        <div className="image-container">
+          <img src={likesImage} alt={name} />
+        </div>
+      </div>
+      {description}
     </div>
   );
 };
--- src/pages/PenguinsPage/PenguinsPageStyles.ts
+++ src/pages/PenguinsPage/PenguinsPageStyles.ts
@@ -1,34 +1,16 @@
 import styled from "styled-components";
-import image from "../../images/background.png";
 
 const PenguinsPageStyles = styled.div`
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
 
-  background-image: url(${image});
   background-size: 200%;
   background-position-x: 80px;
   background-position-y: 230px;
   background-repeat: no-repeat;
   height: 650px;
-  text-align: center;
+  text-align: justify;
 
-  .container {
-    width: 100%;
-  }
-  h1 {
-    top: 5px;
-    position: absolute;
-    width: 100%;
-    font-size: 18px;
-  }
-
-  .link {
-    margin: auto;
-  }
-  a {
-    text-decoration: none;
-  }
   form {
     display: flex;
     flex-direction: column;
@@ -38,54 +20,131 @@ const PenguinsPageStyles = styled.div`
     width: 70%;
     margin: auto;
     padding-top: 105px;
+  }
 
-    label {
-      display: flex;
-      flex-direction: column;
-      width: 80%;
-      height: 60px;
-      padding-bottom: 10px;
-      font-size: 20px;
-      margin: auto;
+  ul,
+  li {
+    list-style-type: none;
+    text-align: left;
+  }
 
-      input {
-        ::placeholder {
-          font-weight: bolder;
-          padding-left: 20px;
-          text-align: center;
-        }
+  .category {
+    background-color: #f4ba26;
+    border-radius: 4px;
+    flex: 1;
+    margin-bottom: 25px;
+    padding-left: 5px;
+  }
 
-        border: 2px solid black;
-        align-items: center;
-        justify-content: center;
-        border-radius: 12px;
-        box-sizing: border-box;
-        color: black;
-        font-size: 18px;
-        height: 100%;
-      }
-    }
-    .submitContainer {
-      display: flex;
-      width: 100%;
+  .likes {
+    text-align: right;
+    flex: 3;
+  }
 
-      button {
-        color: white;
-        background-color: black;
-        padding: 14px 20px;
-        border: none;
-        font-weight: bold;
-        cursor: pointer;
-        width: 80%;
-        opacity: 0.9;
-        display: flex;
-        justify-content: center;
-        font-size: 20px;
-        border-radius: 15px;
-        align-items: center;
-        margin: auto;
-      }
-    }
+  img {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 10px;
+  }
+
+  .star-image {
+    width: 32px;
+    height: 32px;
+    margin-top: 20px;
+  }
+
+  .container {
+    width: 100%;
+  }
+  .penguin-container {
+    border: 1px solid #e4e4e4;
+    border-radius: 8px;
+    margin-bottom: 20px;
+    padding: 16px;
+    padding: 0 10px 15px;
+  }
+
+  .penguin-datalist,
+  .penguin-title {
+    display: flex;
+  }
+
+  .penguin-name,
+  .likes-image {
+    flex: 3;
+  }
+
+  .image-container {
+    text-align: right;
+    margin-right: 5px;
+    width: 32px;
+    height: 32px;
+  }
+
+  .likes-image {
+    width: 32px;
+    height: 32px;
+  }
+  .link {
+    margin: auto;
+  }
+
+  h1,
+  h2 {
+    text-align: left;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    height: 60px;
+    padding-bottom: 10px;
+    font-size: 20px;
+    margin: auto;
+  }
+
+  input {
+    border: 2px solid black;
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px;
+    box-sizing: border-box;
+    color: black;
+    font-size: 18px;
+    height: 100%;
+  }
+
+  input ::placeholder {
+    font-weight: bolder;
+    padding-left: 20px;
+    text-align: center;
+  }
+
+  .submitContainer {
+    display: flex;
+    width: 100%;
+  }
+
+  button {
+    color: white;
+    background-color: black;
+    padding: 14px 20px;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    width: 80%;
+    opacity: 0.9;
+    display: flex;
+    justify-content: center;
+    font-size: 20px;
+    border-radius: 15px;
+    align-items: center;
+    margin: auto;
   }
 `;
 
