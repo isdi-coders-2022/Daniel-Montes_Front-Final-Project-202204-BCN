a342335500fedfe2d6e532741ab4f7da42412d37
Daniel Montes <daniel.montes@gmail.com>
2022-06-08 19:43:30 +0200

    Fix test fail

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import PenguinsPage from "./pages/PenguinsPage/PenguinsPage";
 import FavsPage from "./pages/FavsPage/FavsPage";
+import CheckOutSecurity from "./components/CheckOutSecurity/CheckOutSecurity";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/homepage" />} />
       <Route path="/homepage" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={
+          <CheckOutSecurity>
+            <LoginPage />
+          </CheckOutSecurity>
+        }
+      />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/penguins" element={<PenguinsPage />} />
       <Route path="/favs" element={<FavsPage />} />
--- src/app/redux/store/store.ts
+++ src/app/redux/store/store.ts
@@ -2,9 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../features/userSlice/userSlice";
 import penguinsReducer from "../features/penguinSlice/penguinSlice";
 import uiReducer from "../features/uiSlice/uiSlice";
+import favsReducer from "../features/favsSlice/favsSlice";
 
 const store = configureStore({
-  reducer: { users: usersReducer, penguins: penguinsReducer, ui: uiReducer },
+  reducer: {
+    users: usersReducer,
+    penguins: penguinsReducer,
+    ui: uiReducer,
+    favs: favsReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
