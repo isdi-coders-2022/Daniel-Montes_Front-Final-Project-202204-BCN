8faa2859c623512df2ff461456ea8f9f3918cc4e
Daniel Montes <daniel.montes@gmail.com>
2022-07-11 00:31:40 +0200

    Fix tests failures

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-
 import HomePage from "./HomePage";
+import store from "../../app/redux/store/store";
+import { Provider } from "react-redux";
 
 describe("Given a HomePage component", () => {
   describe("When its rendered", () => {
-    test("Then it should render an image", () => {
-      const expectedText = "AdoptaUnPingüino.com";
+    test("Then it should render an 2 link elements", () => {
       render(
-        <BrowserRouter>
-          <HomePage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <HomePage />
+          </BrowserRouter>
+        </Provider>
       );
       const receivedElement = screen.getByRole("heading");
       const Links = screen.getAllByRole("link");
